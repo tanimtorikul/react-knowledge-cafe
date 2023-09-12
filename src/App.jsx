3ee6a25a585d1b75d278699f86fa9a756dfa07a8
torikulswe <torikulswe@gmail.com>
2023-09-12 14:09:45 +0600

    file setup for react-knowledge-cafe website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Blogs from "./components/Blogs/Blogs";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Blogs />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return <div>
+     <h1 className="text-4xl font-bold text-center">Knowledge cafe</h1>
+  </div>;
+};
+
+export default Header;
